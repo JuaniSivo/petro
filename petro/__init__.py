9e7130c299ab16cdaa627dec6374de0ee6222b8f
JuaniSivo <juan_ignacio_sivori@yahoo.com.ar>
2026-03-22 21:53:47 +0100

    chore: initialise petro repo skeleton

---
+++ petro/__init__.py
@@ -0,0 +1,3 @@
+"""petro — petroleum engineering calculations on top of quantia."""
+
+__version__ = "0.1.0"
---
+++ petro/_utils/__init__.py
@@ -0,0 +1,17 @@
+from petro._utils.dispatch import (
+    _is_prob,
+    _most_uncertain,
+    _coerce,
+    _n_samples,
+    _as_samples,
+    _wrap,
+)
+
+__all__ = [
+    "_is_prob",
+    "_most_uncertain",
+    "_coerce",
+    "_n_samples",
+    "_as_samples",
+    "_wrap",
+]
